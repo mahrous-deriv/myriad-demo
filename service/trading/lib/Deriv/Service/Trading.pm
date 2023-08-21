a3ca436b5eaa5ed7d0b5de4ce06ff308d5e76280
Raunak Kathuria <raunakkathuria@gmail.com>
2023-08-21 14:17:24 +0200

    remove receiver code from the Trading service

--- service/trading/lib/Deriv/Service/Trading.pm
+++ service/trading/lib/Deriv/Service/Trading.pm
@@ -31,8 +31,10 @@ async method diagnostics ($level) {
 
 async method create_order : RPC (%order) {
     return { state => 'fail', content => "Must be defined" } unless keys %order;
+    $log->infof('Creating order %s', %order);
     $order{state} = 'pending';
-    $order{id} = await $self->store_transaction($order{symbol}, $order{amount});
+    await $self->store_transaction($order{symbol}, $order{amount}, );
+    $log->info('Publishing transaction event');
     await $self->publish_transaction_event($order{symbol}, $order{amount}, $order{id});
     return { state => 'success', content => $order{id} };
 }
@@ -41,32 +43,9 @@ method store_transaction (%transaction) {
     return {success => 1, content => $transaction{id}};
 }
 
-async method value_updated : Receiver(service => 'deriv.service.reporting') ($sink, $api, %args) {
-    $log->warnf('Receiver Called | %s | %s | %s', $sink, $api, \%args);
-    my $count = 0;
-    my $sum = 0;
-
-    while (my $e = await $sink) {
-        my %info = $e->@*;
-        $log->infof('INFO %s', \%info);
-
-        my $data = decode_json_utf8($info{'data'});
-        if ($data->{count} == $count + 1) {
-            $sum += $data->{value};
-            $count = $data->{count};
-        } elsif ($data->{count} > $count + 1) {
-            $log->warnf('Skipping data with count %d, expected %d', $data->{count}, $count + 1);
-            $sum = $data->{value};
-            $count = $data->{count};
-        } else {
-            $log->warnf('Ignoring duplicate data with count %d', $data->{count});
-        }
-    }
-}
-
 async method publish_transaction_event ($type, $amount, $transaction_id) {
     my $event_data = {type => $type, amount => $amount, transaction_id => $transaction_id};
-	  $events->emit({transaction => encode_json_utf8($event_data)});
+	$events->emit({transaction => encode_json_utf8($event_data)});
 }
 
 1;
